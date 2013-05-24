aff6edfe54958c3fc1b07bd8c5debf9a724110e1
lsylvester <lachlan.sylvester@hypothetical.com.au>
2013-05-24 17:20:04 +1000

    add some text mate bundles

--- modules/people/manifests/lsylvester.pp
+++ modules/people/manifests/lsylvester.pp
@@ -11,4 +11,24 @@ class people::lsylvester {
   git::config::global{ 'push.default':
     value => 'simple'
   }
+
+  file { '/Users/lachlansylvester/Library/Application Support/TextMate/Managed/Bundles':
+    ensure =>  directory,
+  }
+
+  repository { '/Users/lachlansylvester/Library/Application Support/TextMate/Managed/Bundles/Cucumber.tmbundle':
+    source  => 'cucumber-tmbundle',
+    require => File['/Users/lachlansylvester/Library/Application Support/TextMate/Managed/Bundles']
+  }
+  
+  repository { '/Users/lachlansylvester/Library/Application Support/TextMate/Managed/Bundles/puppet-textmate-bundle':
+    source  => 'masterzen/puppet-textmate-bundle',
+    require => File['/Users/lachlansylvester/Library/Application Support/TextMate/Managed/Bundles']
+  }
+  
+  file { '/Users/lachlansylvester/Library/Application Support/TextMate/Managed/Bundles/Puppet.tmbundle':
+    ensure =>  link,
+    target => '/Users/lachlansylvester/Library/Application Support/TextMate/Managed/Bundles/puppet-textmate-bundle/Puppet.tmbundle'
+  }
+  
 }
